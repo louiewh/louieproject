b0f900da53d6afb86e21538f19e068b94a2c5619
louie.wh <louie.wang.g@gmail.com>
2023-06-17 12:10:59 +0800

    struct array

--- OpenGLProgramming/app/src/main/java/com/louiewh/opengl/render/TriangleRenderVAO.kt
+++ OpenGLProgramming/app/src/main/java/com/louiewh/opengl/render/TriangleRenderVAO.kt
@@ -1,10 +1,10 @@
 package com.louiewh.opengl.render
 
-import com.louiewh.opengl.shader.TriangleShaderVAO
+import com.louiewh.opengl.shader.ShaderStructArray
 
 class TriangleRenderVAO: BaseRender() {
 
     override fun initShader() {
-        shader = TriangleShaderVAO()
+        shader = ShaderStructArray()
     }
 }
